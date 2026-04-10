0d6156ae724709971f0297a3513cf0da1c6e6bcd
DevSchmied <devschmied@yahoo.com>
2026-04-10 15:14:26 +0300

    exercise(Go/datamatrix): add special symbols @!$%^& to test input values

--- go/datamatrix/main.go
+++ go/datamatrix/main.go
@@ -13,8 +13,8 @@ import (
 )
 
 func main() {
-	gasBalloonID := "GB-100045"
-	balloonSerialNumber := "SN-2026-000381"
+	gasBalloonID := "GB-100045@!$%^&"
+	balloonSerialNumber := "SN-2026-000381@!$%^&"
 
 	payload := fmt.Sprintf("%s:%s", gasBalloonID, balloonSerialNumber)
 	outputFile := "datamatrix.png"
